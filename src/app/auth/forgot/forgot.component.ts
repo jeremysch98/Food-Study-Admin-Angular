1840855a2806db48829d375c914e14bda532a59b
jeremysch98 <jeremysch-1510@hotmail.com>
2022-06-09 19:50:15 -0500

    Integración - Login

---
+++ src/app/auth/forgot/forgot.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-forgot',
+  templateUrl: './forgot.component.html',
+  styleUrls: ['./forgot.component.css']
+})
+export class ForgotComponent implements OnInit {
+  title: string = "¿Olvidó su contraseña?";
+  emailFoodStudy: string = "admin@foodstudy.com";
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  loginRedirect() {
+    this.router.navigate(["auth/login"]);
+  }
+
+}
